52c18ae0a15e5be054a8b359b45cbba4901ec950
plotsuda <suda.p@synergysoftware.co.th>
2024-09-13 20:32:14 +0700

    1234

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,22 @@
+import 'package:cmflutter0/src/bloc/login/login_bloc.dart';
 import 'package:cmflutter0/src/pages/home/home_page.dart';
 import 'package:cmflutter0/src/pages/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:cmflutter0/src/pages/login/login_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CMApp extends StatelessWidget {
   const CMApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "CMApp",
-      routes: AppRoute.all,
-      home: LoginPage(),
-    );
+    final loginBloc = BlocProvider(create: (context) => LoginBloc());
+    return MultiBlocProvider(
+        providers: [loginBloc],
+        child: MaterialApp(
+          title: "CMApp",
+          routes: AppRoute.all,
+          home: LoginPage(),
+        ));
   }
 }
